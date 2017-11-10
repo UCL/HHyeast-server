798c61f54a15eaa9e380b7f9b486369952bf4400
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2017-11-10 11:19:31 +0000

    First import of tests. Consolidate template populating scripts.

---
+++ populate_templates.py
@@ -0,0 +1,20 @@
+import sys
+from jinja2 import Template, TemplateNotFound, Environment, FileSystemLoader
+
+
+j2_env = Environment(loader=FileSystemLoader('.'))
+
+rendered_template = j2_env.get_template(
+        'hello.template').render(dnprefix=sys.argv[1])
+with open("hello.py", "w") as f:
+        f.write(rendered_template)
+
+rendered_template = j2_env.get_template(
+        'testing-script.template').render(dnprefix=sys.argv[1])
+with open("testing-script.py", "w") as f:
+        f.write(rendered_template)
+
+rendered_template = j2_env.get_template(
+        'Dockerfile.template').render(dnprefix=sys.argv[1])
+with open("Dockerfile", "w") as f:
+        f.write(rendered_template)
