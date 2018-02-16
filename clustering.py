9426a860840adf2f186336c977ae07a56731cc9b
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2018-02-16 10:09:26 +0000

    Acceptable 'edges-only' algorithm.

--- clustering.py
+++ clustering.py
@@ -10,8 +10,10 @@ class Overlap(Enum):
 
 class Clustering:
     def __init__(self, ov_min=10, ov_min1=0.1, ov_min2=0.5):
-        self._x1cl = []
-        self._x2cl = []
+        self._x1cl_min = []
+        self._x1cl_max = []
+        self._x2cl_min = []
+        self._x2cl_max = []
         self._ncl = []
         self._clabels = []
         self._ov_min = ov_min # Below this number of overlapping residues, there's no overlap
@@ -19,8 +21,10 @@ class Clustering:
         self._ov_min2 = ov_min2 # Above this overlap ratio, there's definitely overlap
 
     def _clean(self):
-        self._x1cl = []
-        self._x2cl = []
+        self._x1cl_min = []
+        self._x1cl_max = []
+        self._x2cl_min = []
+        self._x2cl_max = []
         self._ncl = []
         self._clabels = []
 
@@ -48,7 +52,7 @@ class Clustering:
     def _cluster_id(self, x1, x2):
         o_clusters = ()
         for i in range(len(self._ncl)):
-            overlaps, o_ratio = self._overlap(x1, x2, self._x1cl[i], self._x2cl[i])
+            overlaps, o_ratio = self._overlap(x1, x2, self._x1cl_min[i], self._x1cl_max[i], self._x2cl_min[i], self._x2cl_max[i])
             if overlaps==Overlap.FULL:
                 o_clusters = (o_ratio, i)
                 break
@@ -57,19 +61,23 @@ class Clustering:
                     o_clusters = (o_ratio, i)
         if o_clusters:
             i = o_clusters[1]
-            self._x1cl[i] = min(self._x1cl[i],x1)
-            self._x2cl[i] = max(self._x2cl[i],x2)
+            self._x1cl_min[i] = min(self._x1cl_min[i],x1)
+            self._x1cl_max[i] = max(self._x1cl_max[i],x1)
+            self._x2cl_min[i] = min(self._x2cl_min[i],x2)
+            self._x2cl_max[i] = max(self._x2cl_max[i],x2)
             self._ncl[i] += 1
             return i
-        self._x1cl.append(x1)
-        self._x2cl.append(x2)
+        self._x1cl_min.append(x1)
+        self._x1cl_max.append(x1)
+        self._x2cl_min.append(x2)
+        self._x2cl_max.append(x2)
         self._ncl.append(1)
         return len(self._ncl)-1
 
-    def _overlap(self, x1, x2, x1cl, x2cl):
-        dx_min = float(min(x2-x1, x2cl-x1cl))
-        o_ratio = max(abs(float(x1-x1cl)/dx_min), abs(float(x2-x2cl)/dx_min))
-        if o_ratio<max(self._ov_min1,self._ov_min/dx_min):
-            return Overlap.FULL, o_ratio
+    def _overlap(self, x1, x2, x1cl_min, x1cl_max, x2cl_min, x2cl_max):
+        dx_max = float(x2cl_max-x1cl_min)
+        dx_min = float(x2cl_min-x1cl_max)
+        if x1>(x1cl_min-max(self._ov_min1*dx_max,self._ov_min)) and x1<(x1cl_max+max(self._ov_min1*dx_min,self._ov_min)) and x2>(x2cl_min-max(self._ov_min1*dx_min,self._ov_min)) and x2<(x2cl_max+max(self._ov_min1*dx_max,self._ov_min)):
+            return Overlap.FULL, 0
         else:
-            return Overlap.ZERO, o_ratio
+            return Overlap.ZERO, 0
