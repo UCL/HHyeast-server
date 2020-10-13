02f55be945bb5a1b17f7d17d23a1922ca1d9d898
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2020-10-13 14:42:51 +0100

    Move hhyeast-server.service to the deployment repo.

--- start-server.sh
+++ start-server.sh
@@ -4,3 +4,7 @@ export FLASK_APP=hello.py
 export FLASK_DEBUG=0
 
 nohup gunicorn --bind 0.0.0.0:5000 wsgi &  2>&1
+# Run as a service without gunicorn (just change the username):
+# sudo -u ilektra -E -H -- flask run --host=0.0.0.0 &>/home/ilektra/server.out &
+# Start from command line without gunicorn:
+# nohup flask run --host=0.0.0.0 &  2>&1
